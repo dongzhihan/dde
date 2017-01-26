12b111abf33f1e3830334fdf5bd3d01fc9276fa2
zhihan.dong <529211933@qq.com>
2017-01-26 11:07:19 +0800

    test

--- src/App.vue
+++ src/App.vue
@@ -17,14 +17,15 @@
     }
   }
 </script>
-
+//dasderre
 <style>
   #app {
     font-family: 'Avenir', Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
-   
+    text-align: center;
     color: #2c3e50;
-  
+     
   }
+  
 </style>
